4e069a4f84d125fa130b5a560a9e5f4c55603380
ruge1990 <renhongru1990@qq.com>
2021-01-06 00:28:45 +0100

    update instruction of module rdfquery-----05-01-2021

--- researchee/rdfquery.py
+++ researchee/rdfquery.py
@@ -1,6 +1,10 @@
-#getting name from RDF using sparql
+#This module provides a user interface, which receive a researcher's name, and print his/her expertise out. The interface is running forever, until user type the commnad "exit". In program, sparql is used to query the file "database.rdf"
+#input: name of a researcher
+#output: his/her expertise
+
 import rdflib
-import  requests
+import requests
+
 g = rdflib.Graph()
 
 g.parse("dummy.rdf")
@@ -8,7 +12,7 @@ g.parse("dummy.rdf")
 qres = g.query(
     """SELECT DISTINCT ?name ?expertise
        WHERE {
-         ?a researchee:name ?name .
+         ?a researchee:Name ?name .
          ?a researchee:hasExpertise ?expertise .
        }""")
 print("Name   |  expertise")
