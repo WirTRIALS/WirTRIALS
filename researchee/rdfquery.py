2ac5628e872604921707c36d53d809d9b963c17b
mayuripandey <mayuripandey4@gmail.com>
2021-01-06 21:01:52 +0100

    Infinity loop added with exit

--- researchee/rdfquery.py
+++ researchee/rdfquery.py
@@ -4,21 +4,26 @@ import requests
 
 g = rdflib.Graph()
 g.parse("database.rdf")
+n = 5
+while n > 0:
+    n -= 1
 
-print('Enter your desired name:')
-researchee_name = input()
+    print('\nEnter your desired name:')
+    researchee_name = input()
+    if researchee_name == "exit":
+        break
 
-queryStr = f"""SELECT DISTINCT ?exp 
-          WHERE {{ 
-             ?a researchee:researcherName "{researchee_name}" . 
-             ?a researchee:hasExpertise ?expertise . 
-             ?expertise researchee:expertiseName ?exp . 
-            }}"""
-qres = g.query(queryStr)
+    queryStr = f"""SELECT DISTINCT ?exp 
+              WHERE {{ 
+                 ?a researchee:researcherName "{researchee_name}" . 
+                 ?a researchee:hasExpertise ?expertise . 
+                 ?expertise researchee:expertiseName ?exp . 
+                }}"""
+    qres = g.query(queryStr)
 
-print("Expertise of %s: " %researchee_name)
-if not qres:
-    print("No information available")
-else:
-    for row in qres:
-         print(" %s" % row)
+    print("Expertise of %s: " %researchee_name)
+    if not qres:
+        print("No information available")
+    else:
+        for row in qres:
+             print(" %s" % row)
