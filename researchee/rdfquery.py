0f191c0bb52a6f61a8c950bd69b2321222b8f9c0
mayuripandey <mayuripandey4@gmail.com>
2021-01-06 20:49:56 +0100

    RDF query functionality added: showing expertise name from a resarchhe input name

--- researchee/rdfquery.py
+++ researchee/rdfquery.py
@@ -1,22 +1,24 @@
-#This module provides a user interface, which receive a researcher's name, and print his/her expertise out. The interface is running forever, until user type the commnad "exit". In program, sparql is used to query the file "database.rdf"
-#input: name of a researcher
-#output: his/her expertise
 
 import rdflib
 import requests
 
 g = rdflib.Graph()
+g.parse("database.rdf")
 
-g.parse("dummy.rdf")
+print('Enter your desired name:')
+researchee_name = input()
 
-qres = g.query(
-    """SELECT DISTINCT ?name ?expertise
-       WHERE {
-         ?a researchee:Name ?name .
-         ?a researchee:hasExpertise ?expertise .
-       }""")
-print("Name   |  expertise")
-for row in qres:
-    print("%s  |  %s" % row)
+queryStr = f"""SELECT DISTINCT ?exp 
+          WHERE {{ 
+             ?a researchee:researcherName "{researchee_name}" . 
+             ?a researchee:hasExpertise ?expertise . 
+             ?expertise researchee:expertiseName ?exp . 
+            }}"""
+qres = g.query(queryStr)
 
-    #incomplete still working
+print("Expertise of %s: " %researchee_name)
+if not qres:
+    print("No information available")
+else:
+    for row in qres:
+         print(" %s" % row)
