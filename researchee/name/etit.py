86fbe38933f4b26bf20ea4ae021a33b47e0cceeb
ruge1990 <renhongru1990@qq.com>
2021-02-24 22:31:30 +0100

    update etit.py_24-02-2021

--- researchee/name/etit.py
+++ researchee/name/etit.py
@@ -5,36 +5,40 @@ import requests
 
 def getNameFromETITDept():
     faculty_name = "Electrical Engineering and Information Technology"
-    pro_list = ["https://www.tu-chemnitz.de/etit/dst/professur/team.php.en", "https://www.tu-chemnitz.de/etit/ema/professur/team.php", "https://www.tu-chemnitz.de/etit/leb/staff/mitarbeiter.php.en", "https://www.tu-chemnitz.de/etit/eneho/mitarbeiter/index.html", "https://www.tu-chemnitz.de/etit/hftet/professur/mitarbeiter.php.en", "https://www.tu-chemnitz.de/etit/kn/team/index.php.en", "https://www.tu-chemnitz.de/etit/le/Professur/Mitarbeiter/mitarbeiter.php.en", "https://www.tu-chemnitz.de/etit/nano/members.php"]
-    pro_name = ["Digital Signal Processing and Circuit Technology", "Electrical Energy Conversion Systems and Drives", "Electronic Components", "Power Systems and High-Voltage Engineering", "Microwave Engineering and Electromagnetic Theory", "Communication Networks", "Power Electronics", "Materials for Nanoelectronics"]
+    pro_list = ["https://www.tu-chemnitz.de/etit/dst/professur/team.php.en", "https://www.tu-chemnitz.de/etit/ema/professur/team.php", "https://www.tu-chemnitz.de/etit/leb/staff/mitarbeiter.php.en", "https://www.tu-chemnitz.de/etit/eneho/mitarbeiter/index.html", "https://www.tu-chemnitz.de/etit/hftet/professur/mitarbeiter.php.en", "https://www.tu-chemnitz.de/etit/kn/team/index.php.en", "https://www.tu-chemnitz.de/etit/le/Professur/Mitarbeiter/mitarbeiter.php.en", "https://www.tu-chemnitz.de/etit/nano/members.php", "https://www.tu-chemnitz.de/etit/messtech/team_e.php", "https://www.tu-chemnitz.de/etit/microsys/professur/mitarbeiter.php", "https://www.tu-chemnitz.de/etit/nt/mitarbeiter/index.php.en", "https://www.tu-chemnitz.de/etit/proaut/en/team.html", "https://www.tu-chemnitz.de/etit/control/team/index.php.en", "https://www.tu-chemnitz.de/etit/robosys/professorship/team.php.en", "https://www.tu-chemnitz.de/etit/sse/Mitarbeiter/index.html.en", "https://www.tu-chemnitz.de/etit/microtec/professur/mitarbeiter.php.en", "https://www.tu-chemnitz.de/etit/wetel/professur/mitarbeiter.php.en"]
+    
+    pro_name = ["Digital Signal Processing and Circuit Technology", "Electrical Energy Conversion Systems and Drives", "Electronic Components", "Power Systems and High-Voltage Engineering", "Microwave Engineering and Electromagnetic Theory", "Communication Networks", "Power Electronics", "Materials for Nanoelectronics", "Electrical Measurements and Sensor Technology", "Microsystems and Biomedical Engineering", "Communications Engineering", "Process Automation", "Automatic Control and System Dynamics", "Robotics and Human-Machine Interaction", "Circuit and System Design", "Smart Systems Integration", "Materials and Reliability of Microsystems"]
     
     
     name_list = []
     prof_name = []
-    pro_id = 7
+    pro_id = 0
     while pro_id < len(pro_list):
         r = requests.get(pro_list[pro_id])
         soup = BeautifulSoup(r.text, 'html.parser')
-        if pro_id == 0 or pro_id == 1 or pro_id == 3 or pro_id == 4 or pro_id == 7:
-           prof_name = soup.find_all("div", class_ = "h4")
-        elif pro_id == 2 or pro_id == 5:
-           prof_name = soup.find_all("b")
-        elif pro_id == 6:
-           prof_name = soup.find_all("a", class_ = "collapsed")
-        elif pro_id == 4:
-           prof_name = soup.find_all("div", {'class': 'mitarbeiter'})
-           prof_name = soup.find_all("h3")
-        elif pro_id == 5:
-           prof_name = soup.find_all("div", {'class': 'h4'})
-        elif pro_id == 6:
-           parents = soup.find_all("main", {'class': 'page-content'})
-           for soup_item in parents:
-               prof_name = soup_item.find_all("p")
-        elif pro_id == 7:
-            prof_name = soup.find_all("div", class_ = "h4")
-        else:
-           prof_name = soup.find_all("h4", class_ = "fn")
 
+        if pro_id == 2 or pro_id == 5:
+            prof_name = soup.find_all("b")
+        elif pro_id == 6 or pro_id == 14:
+            prof_name = soup.find_all("a", class_ = "collapsed")
+        elif pro_id == 10:
+            prof_name = soup.find_all("h3", class_ = 'teaser')
+        elif pro_id == 12:
+            table_list = soup.find_all("table")
+            for table in table_list:
+                prof_name.append(table.find("h3", {'style': 'margin:0;'}))
+        elif pro_id == 13:
+            table_list = soup.find_all("div", class_ = "TeamTableCol")
+            for table in table_list:
+                prof_name.append(table.find("strong"))
+        elif pro_id == 15:
+            row_list = soup.find_all("tr")
+            for row in row_list:
+                if row.find("td"):
+                    prof_name.append(row.find_all("td")[0])
+        else:
+            prof_name = soup.find_all("div", class_ = "h4")
+        
 
         for item in prof_name:
             try:
@@ -60,8 +64,10 @@ def getNameFromETITDept():
             name = name.replace("nat.", "")
             name = name.replace("rer.", "")
             name = name.replace("h.c.", "")
+            name = name.replace("MSc.", "")   
             name = name.replace("M.", "")
-            name = name.replace("Sc.", "")   
+            name = name.replace("Sc.", "")
+            name = name.replace("Sci.", "")   
             name = name.replace("B.", "")
             name = name.replace("A.", "")               
             name = name.replace("Ph.D.", "")
@@ -72,7 +78,8 @@ def getNameFromETITDept():
             name = name.replace("MBA", "")
             name = name.replace("Herr", "")
             name = name.replace("(FH)", "")
-            name = name.replace("(BA)", "")                
+            name = name.replace("(BA)", "")
+            name = name.replace(",", "")                
             name = name.lstrip()
             name = name.strip()
             
@@ -111,7 +118,9 @@ def getNameFromETITDept():
             name_list.append(nameAndFaculty)
 
         pro_id += 1
-        
+
     return name_list
-    
-print(getNameFromETITDept())
+
+'''list = getNameFromETITDept()
+for item in list:
+    print(item.split('&')[0] + '\t' + item.split('&')[1])'''
