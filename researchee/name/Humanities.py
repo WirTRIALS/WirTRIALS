8be0c6b524d6e966fbdc0caaea391373a3ed61ed
Swagat Gyawali <unityenator@gmail.com>
2021-03-10 18:27:04 +0100

    Fixed issue of limited names

--- researchee/name/Humanities.py
+++ researchee/name/Humanities.py
@@ -1,4 +1,8 @@
 from ldap3 import Server, Connection, SUBTREE, LEVEL
+from string import ascii_lowercase
+from itertools import product
+import time
+import json
 
 def getNameFromHumanities():
     total_entries = 0
@@ -7,30 +11,39 @@ def getNameFromHumanities():
         'auto_bind': True,
     }
     conn = Connection(**connection_kwargs)
+    pause_counter = 0
+    combos = (''.join(i) for i in product(ascii_lowercase, repeat = 3)) # Looping through characters a-z 
+    for keyword in combos:
+        pause_counter += 1
+        entry_generator = conn.extend.standard.paged_search(search_base = 'ou=Users,dc=tu-chemnitz,dc=de',
+                search_filter= '(& (objectClass=Person) (|(cn=*gaedke*)))',
+                                                        search_scope = SUBTREE,
+                                                        attributes = ['cn', 'ou', 'title','uid'],
+                                                        paged_size= 200,
+                                                        get_operational_attributes=True)  
+        namelist = []
 
-        # paged search wrapped in a generator
-    total_entries = 0
-    entry_generator = conn.extend.standard.paged_search(search_base = 'ou=Users,dc=tu-chemnitz,dc=de',
-            search_filter= '(& (objectClass=Person) (|(ou=*phil*)))',
-                                                    search_scope = SUBTREE,
-                                                    attributes = ['cn', 'ou', 'title'],
-                                                    paged_size= 200,
-                                                    get_operational_attributes=True)  
-    namelist = []
 
-    for entry in entry_generator:
-        total_entries += 1
-        name = ''.join(entry['attributes']['cn'])
-        faculty = "Humanities"
-        professorship = ''.join(entry['attributes']['ou'][0])
-        professorship_ = professorship.split("|")[0]
-        title = "Researcher"
-        if ''.join(entry['attributes']['title']) == "Prof.":
-            title = "Professor"
+        for entry in entry_generator:
+            namse = ''.join(entry['attributes']['uid'])
+            total_entries += 1
+            name = ''.join(entry['attributes']['cn'])
+            faculty = "Humanities"
+            professorship = ''.join(entry['attributes']['ou'][0])
+            professorship_ = professorship.split("|")[-1]
+            title = "Researcher"
+            if ''.join(entry['attributes']['title']) == "Prof.":
+                title = "Professor"
+            
+            nameAndFaculty = name + '&' + title + '&' + professorship_
+            namelist.append(nameAndFaculty)
+            print(nameAndFaculty)
         
-        nameAndFaculty = name + '&' + title + '&' + professorship_
-        namelist.append(nameAndFaculty)
-    print(namelist)
-    return(namelist)
+        if pause_counter == 2:
+            delay = 0.1
+            time.sleep(delay)
+            pause_counter = 0
+
+    
 
 getNameFromHumanities()
